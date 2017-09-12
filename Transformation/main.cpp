87805db03c44f8d7ed2977898c8cf352c8807d84
Arif F L <lazuardi.dp@gmail.com>
2017-09-12 18:52:44 +0700

    tranformation update in my pc

--- Transformation/main.cpp
+++ Transformation/main.cpp
@@ -6,71 +6,161 @@
 
 /* 
  * File:   main.cpp
- * Author: RIF <arif.lazuardi@infoglobal.co.id>
+ * Author: rif
  *
- * Created on September 4, 2017, 9:34 AM
+ * Created on September 4, 2017, 11:21 AM
  */
 
-#include <cstdlib>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <cstdlib>
+#include <limits>
 
 using namespace std;
 
-void normalize_point_at_center(int width, int height, cv::Point *point) {
-    point->x = point->x + width/2;
-    point->y = point->y + height/2;
+cv::String WINDOW_NAME = "Transformation";
+
+void normalize_point(int width, int height, cv::Point *point) {
+    point->x = (width/2) + point->x;
+    point->y = (height/2) - point->y;
 }
 
-void create_line(cv::Mat im, cv::Point start, cv::Point end) {
-    int thickness = 1;
-    int lineType = cv::LINE_8;
+vector<cv::Point> create_vector_point(cv::Point bottom_left, int width, int height) {
+    cv::Point pt_1, pt_2, pt_3, pt_4, pt_5;
     
-    normalize_point_at_center(im.cols,im.rows,&start);
-    normalize_point_at_center(im.cols,im.rows,&end);
-    cv::line( im, start, end, cv::Scalar( 0, 0, 0 ), thickness, lineType );
+    pt_1.x = bottom_left.x; pt_1.y = bottom_left.y;
+    pt_2.x = bottom_left.x+width; pt_2.y = bottom_left.y;
+    pt_3.x = bottom_left.x+width; pt_3.y = bottom_left.y+height;
+    pt_4.x = bottom_left.x; pt_4.y = bottom_left.y+height;
+    pt_5.x = bottom_left.x; pt_5.y = bottom_left.y;
+  
+    vector<cv::Point> vector_pt;
+    vector_pt.push_back(pt_1);
+    vector_pt.push_back(pt_2);
+    vector_pt.push_back(pt_3);
+    vector_pt.push_back(pt_4);
+    vector_pt.push_back(pt_5);
+    
+    return vector_pt;
 }
 
-void create_rectangle(cv::Mat im, vector<cv::Point> points) {
-    for(int i=0; i<points.size()-1; i++) {
-        create_line(im,points[i],points[i+1]);
+void create_line(cv::Mat im, cv::Point start, cv::Point end, cv::Scalar color) {
+    int thickness = 1;
+    int line_type = cv::LINE_8;
+    
+    normalize_point(im.cols,im.rows,&start);
+    normalize_point(im.cols,im.rows,&end);
+    
+    cv::line(im,start,end,color,thickness,line_type);
+}
+
+void draw_rectangle(cv::Mat im, vector<cv::Point> vector_pt, cv::Scalar color) {
+    for(int i=0; i<vector_pt.size()-1; i++) {
+        create_line(im,vector_pt[i],vector_pt[i+1],color);
     }
+}
+
+vector<cv::Point> m_rotasi(vector<cv::Point> vect_pt, double theta) {
+    cv::Mat M(3,3,CV_64F);
+    
+    M.at<float>(0,0) = cos(theta);
+    M.at<float>(0,1) = -sin(theta);
+    M.at<float>(0,2) = 0;
+    
+    M.at<float>(1,0) = sin(theta);
+    M.at<float>(1,1) = cos(theta);
+    M.at<float>(1,2) = 0;
+    
+    M.at<float>(2,0) = 0;
+    M.at<float>(2,1) = 0;
+    M.at<float>(2,2) = 1;
+    
+    for(int i=0; i<vect_pt.size(); i++) {
+        vect_pt[i].x = (M.at<float>(0,0)*vect_pt[i].y) + (M.at<float>(0,1)*vect_pt[i].x) + M.at<float>(0,2);
+        vect_pt[i].y = (M.at<float>(1,0)*vect_pt[i].y) + (M.at<float>(1,1)*vect_pt[i].x) + M.at<float>(1,2);
+    }
+    return vect_pt;
+}
+
+vector<cv::Point> m_translasi(vector<cv::Point> vect_pt, int delta_x, int delta_y) {
+    cv::Mat M(3,3,CV_64F);
+    
+    M.at<float>(0,0) = 1;
+    M.at<float>(0,1) = 0;
+    M.at<float>(0,2) = delta_x;
+    
+    M.at<float>(1,0) = 0;
+    M.at<float>(1,1) = 1;
+    M.at<float>(1,2) = delta_y;
+    
+    M.at<float>(2,0) = 0;
+    M.at<float>(2,1) = 0;
+    M.at<float>(2,2) = 1;
+    
+    for(int i=0; i<vect_pt.size(); i++) {
+        vect_pt[i].x = M.at<float>(0,0)*vect_pt[i].x + M.at<float>(0,1)*vect_pt[i].y + M.at<float>(0,2);
+        vect_pt[i].y = M.at<float>(1,0)*vect_pt[i].x + M.at<float>(1,1)*vect_pt[i].y + M.at<float>(1,2);
+    }
+    return vect_pt;
+}
+
+vector<cv::Point>  m_skala(vector<cv::Point> vect_pt, int s_x, int s_y) {
+    cv::Mat M(3,3,CV_64F);
+    
+    M.at<float>(0,0) = s_x;
+    M.at<float>(0,1) = 0;
+    M.at<float>(0,2) = 0;
+    
+    M.at<float>(1,0) = 0;
+    M.at<float>(1,1) = s_y;
+    M.at<float>(1,2) = 0;
+    
+    M.at<float>(2,0) = 0;
+    M.at<float>(2,1) = 0;
+    M.at<float>(2,2) = 1;
+    
+    for(int i=0; i<vect_pt.size(); i++) {
+        vect_pt[i].x = M.at<float>(0,0)*vect_pt[i].x + M.at<float>(0,1)*vect_pt[i].y + M.at<float>(0,2);
+        vect_pt[i].y = M.at<float>(1,0)*vect_pt[i].x + M.at<float>(1,1)*vect_pt[i].y + M.at<float>(1,2);
+    }
+    return vect_pt;
 }
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    //BASIC CONTAINER
-    cv::Mat container(480,720,CV_64FC3,cv::Scalar(255,255,255));
+    //CONTAINER
+    cv::Mat container(480,720,CV_64FC3,cv::Scalar(0,0,0));
     
-    //DRAW LINE
-    cv::Point pt_1, pt_2, pt_3, pt_4, pt_5;
-    pt_1.x = 0; pt_1.y = 0;
-    pt_2.x = 20; pt_2.y = 0; 
-    pt_3.x = 20; pt_3.y = 20;
-    pt_4.x = 0; pt_4.y = 20;
-    pt_5.x = 0; pt_5.y = 0;
+    cv::Scalar RED(0,0,255);
+    cv::Scalar BLUE(255,0,0);
+    cv::Scalar WHITE(255,255,255);
+    cv::Point start(-30,-30);
     
-    vector<cv::Point> vector_point;
-    vector_point.push_back(pt_1);
-    vector_point.push_back(pt_2);
-    vector_point.push_back(pt_3);
-    vector_point.push_back(pt_4);
-    vector_point.push_back(pt_5);
+    vector<cv::Point> vect_point;
     
-
+    vect_point = m_rotasi(create_vector_point(start,60,60),45);
+    draw_rectangle(container,vect_point,RED);
     
-    create_rectangle(container,vector_point);
+    vect_point = m_translasi(create_vector_point(start,40,40),120,120);
+    draw_rectangle(container,vect_point,BLUE);
     
-    //DRAW
-    cv::namedWindow("Transformation",cv::WINDOW_AUTOSIZE);
-    cv::imshow("Transformation",container);
+    vect_point = m_translasi(create_vector_point(start,20,20),170,170);
+    draw_rectangle(container,vect_point,WHITE);
+    
+    /**
+    vector<cv::Point> vect_rotasi = m_rotasi(create_vector_point(start,20,20),30);
+    vect_translasi = m_translasi(vect_rotasi,0,120);
+    draw_rectangle(container,vect_translasi,WHITE);*/
+    
+    
+    
+    //DISPLAY
+    cv::namedWindow(WINDOW_NAME,cv::WINDOW_AUTOSIZE);
+    cv::imshow(WINDOW_NAME,container);
     cv::waitKey(0);
-    //END DRAW
     return 0;
 }
-
-
 
